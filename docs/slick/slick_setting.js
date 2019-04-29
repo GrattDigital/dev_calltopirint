e304bb702619df660b9baf94f03e6ea67d8be152
Vladimir Sh <gratt.shvv@gmail.com>
2019-04-30 01:37:07 +0300

    add slick folder

---
+++ docs/slick/slick_setting.js
@@ -0,0 +1,16 @@
+$('.slider_for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider_nav'
+});
+$('.slider_nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider_for',
+  dots: false,
+  arrows: true,
+  centerMode: true,
+  focusOnSelect: true
+});
